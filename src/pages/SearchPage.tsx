19dfb64108417f4c9e8a50e5915d0203578a64bf
valentine OO <varlomxg@gmail.com>
2024-04-30 18:21:37 +0100

    search page layout

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,21 +1,25 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results } = useSearchRestaurants(city);
+  const { results, isLoading } = useSearchRestaurants(city);
 
+  if (isLoading) {
+    <span>Loading ...</span>; //TODO
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>; //loading
+  }
   return (
-    <span>
-      User search for {city}{" "}
-      <span>
-        {results?.data.map((restaurant) => (
-          <span>
-            found - {restaurant.restaurantName}, {restaurant.city}
-          </span>
-        ))}
-      </span>
-    </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">Insert cuisines here:</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+      </div>
+    </div>
   );
 };
 
